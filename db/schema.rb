c4686ed6a558f9a503ba0020ff28340518def1dc
Christina Chortaria <actspatial@gmail.com>
2022-06-01 15:54:01 -0400

    Remove old migration Update the task as a different rake task. Include fixtures in alma fixtures.

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_203055) do
+ActiveRecord::Schema.define(version: 2022_06_01_195523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_203055) do
     t.integer "count"
     t.text "sort"
     t.string "dir"
+    t.string "vocabulary"
     t.index ["sort"], name: "index_alma_orangelight_subjects_on_sort"
   end
 
@@ -63,7 +64,6 @@ ActiveRecord::Schema.define(version: 2021_08_02_203055) do
     t.datetime "updated_at"
     t.string "document_type"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
-    t.string "vocabulary"
   end
 
   create_table "searches", id: :serial, force: :cascade do |t|
